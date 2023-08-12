be1a58d0c6b82d515ae9f6aefeb8ba68d7eee4cb
Ewan-Roberts <jojothegreat694@hotmail.com>
2023-08-12 16:45:58 -0300

    small visual updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // comment out strict mode as this will only ever be run locally and removes the dev environement double loading of
+  // the page
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
